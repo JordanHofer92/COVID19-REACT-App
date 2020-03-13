840992813098cea2830134a2f1732cceba970ce9
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-03-13 11:03:14 -0600

    first commit

---
+++ src/Components/Components-NavBar/Components-NavBar-Button/Components-NavBar-Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Components-NavBar-Button.css"
+
+const Button = () => {
+    return (
+        <button className= "">Click</button>
+    )
+}
+
+export default Button
