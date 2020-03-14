87fd6bcd22b81aa5d662b06f2e6e7d1320d2a826
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-03-13 22:23:08 -0600

    made a clickable world map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Map from './Components/Components-Map/Components-Map.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hey</h1>
+        <Map />
       </header>
     </div>
   );
