547aa2b4d208ecc5cb91ae1194ab7bb91bf9d8c2
JordanHofer92 <jhofer@ualberta.ca>
2020-03-16 17:08:50 -0600

    Cleaned up comments from generic files, added chartjs, made BarGraph and Canvas components to go into Chart component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import Map from './Components/Components-Map/Components-Map.js'
+// import Map from './Components/Components-Map/Components-Map.js'
 // import API from "./Components/API-Retrieve/API.js";
 // import ComponentPage1 from "./Components/Components-Page1/ComponentPage1.js";
+import Chart from './Components/Components-Chart/ComponentsChart.js'
 
 function App(allInfo) {
   console.log(allInfo)
@@ -12,8 +13,10 @@ function App(allInfo) {
     <div className="App">
       {/* <ComponentPage1 /> */}
       <header className="App-header">
-        <p>{JSON.stringify(allInfo.allInfo[0])}</p>
-        <Map />
+      
+        <Chart />
+        {/* <p>{JSON.stringify(allInfo.allInfo[0])}</p> */}
+        {/* <Map /> */}
         {/* <API /> */}
       </header>
     </div>
---
+++ src/Components/Components-Chart/ComponentsChart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Canvas from './Canvas.js'
+import BarGraph from './bargraph.js'
+
+const ComponentsChart = () => {
+    return (
+        <div>
+            <Canvas />
+            <BarGraph/>
+        </div>
+    )
+}
+
+export default ComponentsChart
