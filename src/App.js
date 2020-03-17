0b4746ce9914d1a77e10fef750882e65616de279
Ivan Wang <ivanhaifengwang@users.noreply.github.com>
2020-03-17 11:31:42 -0600

    working on 'onClick'

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import "./App.css";
 import Map from "./Components/Components-Map/Components-Map.js";
 import ComponentPage1 from "./Components/Components-Page1/ComponentPage1.js";
 
-function App(allInfo) {
-  console.log(allInfo);
-  console.log(allInfo.allInfo[0]);
+function App(props) {
+  // console.log(props);
+  // console.log(props.allInfo[0]);
 
   return (
     <div className="App">
       <ComponentPage1 />
       <header className="App-header">
-        <p>{JSON.stringify(allInfo.allInfo[0])}</p>
-        <Map />
+        {/* <p>{JSON.stringify(props.allInfo[0])}</p> */}
+        <Map allInfo = {props.allInfo}/>
       </header>
     </div>
   );
