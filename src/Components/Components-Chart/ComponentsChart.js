ca2ee70bc47b7cf001be79e26ace5aaa4a49dc06
Kevin Crawford <gadgecrawford@hotmail.com>
2020-03-17 14:33:05 -0600

    Added countries

--- src/Components/Components-Chart/ComponentsChart.js
+++ src/Components/Components-Chart/ComponentsChart.js
@@ -1,8 +1,8 @@
-import React from "react";
-//import BarGraph from './BarGraph.js'
+//import React from "react";
+////import BarGraph from './BarGraph.js'
 
-const ComponentsChart = () => {
-  return <div>{/*<BarGraph />*/}</div>;
-};
+//const ComponentsChart = () => {
+//  return <div>{/*<BarGraph />*/}</div>;
+//};
 
-export default ComponentsChart;
+//export default ComponentsChart;
