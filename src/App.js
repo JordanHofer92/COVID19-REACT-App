ddd701a539bb158f74414754853ba2a77f245ce9
JordanHofer92 <jhofer@ualberta.ca>
2020-03-18 10:48:54 -0600

    roughed out watchlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-import Map from "./Components/Components-Map/Components-Map.js";
-import ComponentPage1 from "./Components/Components-Page1/ComponentPage1.js";
+// import Map from "./Components/Components-Map/Components-Map.js";
+// import ComponentPage1 from "./Components/Components-Page1/ComponentPage1.js";
 // import Chart from './Components/Components-Chart/BarGraph/BarGraph.js'
 import NavBar from './Components/Components-NavBar/NavBar.js'
-import CheckList from './Components/Components-CheckList/CheckList.js'
+// import CheckList from './Components/Components-CheckList/CheckList.js'
+// import WatchList from './Components/Components-WatchList/WatchList.js'
 
 
 function App(props) {
@@ -17,11 +18,12 @@ function App(props) {
     <div className="App">
       {/* <header className="App-header">
       </header> */}
-        <ComponentPage1 />
+        {/* <ComponentPage1 /> */}
         {/* <Chart /> */}
-        <CheckList /> 
+        {/* <CheckList />  */}
         {/* <p>{JSON.stringify(allInfo.allInfo[0])}</p> */}
-        <Map allInfo={props.allInfo}/>
+        {/* <Map allInfo={props.allInfo}/> */}
+        {/* <WatchList/> */}
         <NavBar/>
     </div>
   );
