49c4955edc412702a468e581aec965715204d703
JordanHofer92 <jhofer@ualberta.ca>
2020-03-18 14:13:24 -0600

    Router working

---
+++ src/Components/Components-LocalNews/LocalNews.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LocalNews = () => {
+    return (
+        <div>
+            <ul>
+                <li>Articles</li>
+                <li>Articles</li>
+                <li>Articles</li>
+                <li>Articles</li>
+                <li>Articles</li>
+            </ul>
+        </div>
+    )
+}
+
+export default LocalNews
--- src/Components/Components-NavBar/NavBar.js
+++ src/Components/Components-NavBar/NavBar.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import './NavBar.css'
-import MapButton from './MapButton/MapButton.js'
-import ChartButton from './ChartButton/ChartButton.js'
-import CheckListButton from './CheckListButton/CheckListButton.js'
-import LocalNewsButton from './LocalNewsButton/LocalNewsButton.js'
-import WatchListButton from './WatchListButton/WatchListButton.js'
+// import React from 'react'
+// import { BrowserRouter as Router, Link } from "react-router-dom";
+// import './NavBar.css'
+// import MapButton from './MapButton/MapButton.js'
+// import ChartButton from './ChartButton/ChartButton.js'
+// import CheckListButton from './CheckListButton/CheckListButton.js'
+// import LocalNewsButton from './LocalNewsButton/LocalNewsButton.js'
+// import WatchListButton from './WatchListButton/WatchListButton.js'
 
 
-const NavBar = () => {
-    return (
-        <div className="NavButtons">
-            <MapButton/>
-            <ChartButton/>
-            <CheckListButton/>
-            <LocalNewsButton/>
-            <WatchListButton/>
-        </div>
-    )
-}
+// const NavBar = () => {
+//     return (
+//     <Router>
+//         <nav className="NavButtons">
+//             <Link to="/map"><MapButton/></Link>
+//             <Link to="/chart"><ChartButton/></Link>
+//             <Link to="/checkList"><CheckListButton/></Link>
+//             <Link to="/localNews"><LocalNewsButton/></Link>
+//             <Link to="/watchList"><WatchListButton/></Link>
+//         </nav>
+//     </Router> 
+//     )
+// }
 
-export default NavBar
+// export default NavBar
+
+
