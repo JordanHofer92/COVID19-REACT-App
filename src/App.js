1934e5eb46352b1551cacfb1d33a92ebccb8471d
JordanHofer92 <jhofer@ualberta.ca>
2020-03-17 21:48:20 -0600

    Roughed out checklist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-// import Map from "./Components/Components-Map/Components-Map.js";
+import Map from "./Components/Components-Map/Components-Map.js";
 import ComponentPage1 from "./Components/Components-Page1/ComponentPage1.js";
 import Chart from './Components/Components-Chart/BarGraph/BarGraph.js'
 import NavBar from './Components/Components-NavBar/NavBar.js'
+import CheckList from './Components/Components-CheckList/CheckList.js'
 
 
 function App(props) {
@@ -11,13 +13,15 @@ function App(props) {
   // console.log(props.allInfo[0]);
 
   return (
+    
     <div className="App">
       {/* <header className="App-header">
       </header> */}
         <ComponentPage1 />
         <Chart />
+        <CheckList /> 
         {/* <p>{JSON.stringify(allInfo.allInfo[0])}</p> */}
-        {/* <Map /> */}
+        <Map />
         <NavBar/>
     </div>
   );
