e7d13597ad8dd7b523cd22d6ee038c9d902022a7
Kevin Crawford <gadgecrawford@hotmail.com>
2020-03-19 12:04:46 -0600

    fixing errors with checklist

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import "./App.css";
 import Map from "./Components/Components-Map/Components-Map.js";
 import ComponentPage1 from "./Components/Components-Header/Header.js";
-import Chart from './Components/Components-Chart/BarGraph/BarGraph.js'
-import MapButton from './Components/Components-NavBar/MapButton/MapButton.js'
-import ChartButton from './Components/Components-NavBar/ChartButton/ChartButton.js'
-import CheckListButton from './Components/Components-NavBar/CheckListButton/CheckListButton.js'
-import LocalNewsButton from './Components/Components-NavBar/LocalNewsButton/LocalNewsButton.js'
-import WatchListButton from './Components/Components-NavBar/WatchListButton/WatchListButton.js'
-import CheckList from './Components/Components-CheckList/CheckList.js'
-import WatchList from './Components/Components-WatchList/WatchList.js'
-import LocalNews from './Components/Components-LocalNews/LocalNews.js'
-
+import Chart from "./Components/Components-Chart/BarGraph/BarGraph.js";
+import MapButton from "./Components/Components-NavBar/MapButton/MapButton.js";
+import ChartButton from "./Components/Components-NavBar/ChartButton/ChartButton.js";
+import CheckListButton from "./Components/Components-NavBar/CheckListButton/CheckListButton.js";
+import LocalNewsButton from "./Components/Components-NavBar/LocalNewsButton/LocalNewsButton.js";
+import WatchListButton from "./Components/Components-NavBar/WatchListButton/WatchListButton.js";
+import CheckList from "./Components/Components-CheckList/CheckList.js";
+import WatchList from "./Components/Components-WatchList/WatchList.js";
+import LocalNews from "./Components/Components-LocalNews/LocalNews.js";
 
 function App(props) {
   // console.log(props);
@@ -21,30 +20,40 @@ function App(props) {
   return (
     <Router>
       <div className="App">
-          <ComponentPage1 />
-          <Switch>
-            <Route path="/chart">
-              <Chart />
-            </Route>
-            <Route path="/checkList">
-              <CheckList /> 
-            </Route>
-            <Route path="/map">
-              <Map allInfo={props.allInfo}/>
-            </Route>
-            <Route path="/watchList">
-              <WatchList/>
-            </Route>
-            <Route path="/localNews">
-              <LocalNews/>
-            </Route>
-          </Switch>
+        <ComponentPage1 />
+        <Switch>
+          <Route path="/chart">
+            <Chart />
+          </Route>
+          <Route path="/checkList">
+            <CheckList />
+          </Route>
+          <Route path="/map">
+            <Map allInfo={props.allInfo} />
+          </Route>
+          <Route path="/watchList">
+            <WatchList />
+          </Route>
+          <Route path="/localNews">
+            <LocalNews />
+          </Route>
+        </Switch>
         <nav className="NavButtons">
-          <Link to="/map"><MapButton/></Link>
-          <Link to="/chart"><ChartButton/></Link>
-          <Link to="/checkList"><CheckListButton/></Link>
-          <Link to="/localNews"><LocalNewsButton/></Link>
-          <Link to="/watchList"><WatchListButton/></Link>
+          <Link to="/map">
+            <MapButton />
+          </Link>
+          <Link to="/chart">
+            <ChartButton />
+          </Link>
+          <Link to="/checkList">
+            <CheckListButton />
+          </Link>
+          <Link to="/localNews">
+            <LocalNewsButton />
+          </Link>
+          <Link to="/watchList">
+            <WatchListButton />
+          </Link>
         </nav>
       </div>
     </Router>
--- src/Components/Components-NavBar/CheckListButton/CheckListButton.js
+++ src/Components/Components-NavBar/CheckListButton/CheckListButton.js
@@ -1,15 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Checklist from "./Checklist/Checklist.js";
 const CheckListButton = () => {
+  return (
+    <button>
+      <i className="fas fa-tasks"></i>
+      <br></br>
+      Checklist
+    </button>
+  );
+};
 
-    return (
-        <button>
-            <i className="fas fa-tasks"></i><br></br>
-            Checklist
-        </button>
-
-        
-    )
-}
-
-export default CheckListButton
+export default CheckListButton;
